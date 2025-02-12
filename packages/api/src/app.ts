a0853300bf98e16ef9a393e4b6099e7092173e4e
Viljar Võidula <viljar@testreel.com>
2025-02-12 13:34:13 +0200

    chore: monorepo migration

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -2,8 +2,8 @@ import http from 'http';
 import express from 'express';
 import cors from 'cors';
 
-import health from './src/routes/health';
-import { remoteWorker } from './src/examples/puppetmaster';
+import health from './routes/health';
+import { remoteWorker } from '@forgemaster-workers/core/src/examples/puppetmaster';
 
 const app = express();
 
@@ -18,7 +18,6 @@ app.use('/health', health);
 const server = http.createServer(app);
 
 remoteWorker.init();
-
 
 server.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}`);
